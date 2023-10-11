aa4ebceccf7632d1625d5ec65e1c2440be4cc1d7
aswanijehangeer <jahangiraswani237@gmail.com>
2023-10-11 16:41:13 +0500

    global.R

--- global.R
+++ global.R
@@ -5,6 +5,8 @@ library(tidyverse)
 library(bslib)
 library(echarts4r)
 library(shinycssloaders)
+library(shinyWidgets)
+library(shinyalert)
 
 # Importing data set (In data/ folder)
 cereal_data <- readxl::read_xlsx("data/index-of-cereal-production-yield-and-land-use.xlsx") |> 
