d98eaec668f458bc269c0260dec1eed7512176d8
aswanijehangeer <jahangiraswani237@gmail.com>
2023-10-11 11:07:59 +0500

    ui.R

--- ui.R
+++ ui.R
@@ -1,7 +1,22 @@
 ui <- page_sidebar(
+  # Welcome pop-up modal
+  modalDialog(
+    h5("This app provides an overview of global cereal production from 1961 to 2021."),
+    br(),
+    h5("The values shown in the boxes are static and are calculated based on data for all countries available in the dataset."),
+    br(),
+    h5("NOTE: In future updates, interactive features will be added for the value boxes."),
+    title = "Welcome to Global Cereal Production App",
+    size = "l",
+    easyClose = FALSE,
+  ),
+  # Title of the App
   title = "Global Cereal Production [1961 - 2021]",
+  theme = bs_theme(bootswatch = "minty"),
   
+  # Side bar
   sidebar = sidebar(
+    # Inputs Controls
     selectInput("country", "Choose Country",unique(cereal_data$entity),
                 selected = "Africa"),
     sliderInput("year", "Choose Years",
@@ -9,22 +24,26 @@ ui <- page_sidebar(
                 max = max(cereal_data$year),
                 value = c(1961, 2021))
   ),
-  
+  # Value boxes layout
   layout_columns(
     fill = FALSE,
     value_box(
+      # style = 'background-color: #5C7BD9!important;',
       title = "Total Area Harvested across all Countries (Hectares)",
       value = paste0(round(sum(cereal_data$area_harvested_hectares, na.rm = TRUE) / 1e9, 1), " Bn")),
     value_box(
-      title = "Total Production across all Countries (Tonnes)",
+      # style = 'background-color: #FFDC60!important;',
+      title = "Total Cereal Production across all Countries (Tonnes)",
       value = paste0(round(sum(cereal_data$production_tonnes, na.rm = TRUE) / 1e9, 1), " Bn")),
     value_box(
-      title = "Total Yeild across all Countries (Hectares)",
-      value = paste0(round(sum(cereal_data$yield_tonnes_per_hectare, na.rm = TRUE),1)), " Bn")
+      # style = 'background-color: #A0E081!important;',
+      title = "Total Cereal Yeild across all Countries (Tonnes)",
+      value = round(sum(cereal_data$yield_tonnes_per_hectare, na.rm = TRUE),1))
   ),
   
+  # Main panel plot card
   card(
-    card_header("Cereal and the Green Revolution: Change in production vs. 1961"),
+    card_header("% Change of Cereal Production in the Countries since 1961"),
     echarts4rOutput("plot")
   )
 )
