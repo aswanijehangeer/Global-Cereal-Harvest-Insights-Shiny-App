876a772524b3567462a54cbeaa5921a7f505e8df
aswanijehangeer <jahangiraswani237@gmail.com>
2023-10-11 09:32:34 +0500

    global.R

--- global.R
+++ global.R
@@ -1,2 +1,11 @@
 library(shiny)
 library(tidyverse)
+library(bslib)
+library(echarts4r)
+library(shinycssloaders)
+
+
+cereal_data <- readxl::read_xlsx("data/index-of-cereal-production-yield-and-land-use.xlsx") |> 
+  janitor::clean_names()
+
+
